fe605fbd49df1310c7e2e65192bbcebd9c0d1845
Apple <apple@Apples-Mac.fritz.box>
2016-08-03 14:59:40 +1200

    first tenative server

--- bin/loop-server-fast.js
+++ bin/loop-server-fast.js
@@ -324,7 +324,8 @@ function parseCookies (request) {
 function ago(mysqlTimeStr) {
    //Time ago
    // Split timestamp into [ Y, M, D, h, m, s ]
-   var t = mysqlTimeStr.split(/[- :]/);
+   console.log(JSON.stringify(mysqlTimeStr));
+   var t = JSON.stringify(mysqlTimeStr).split(/[- :]/);
 
  // Apply each element to the Date function
   var d = new Date(Date.UTC(t[0], t[1]-1, t[2], t[3], t[4], t[5]));
