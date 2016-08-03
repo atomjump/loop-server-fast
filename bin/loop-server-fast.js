2f1af50278f433ddf2a348400d3044fdf7c45434
Apple <apple@Apples-Mac.fritz.box>
2016-08-03 15:50:14 +1200

    first tenative server

--- bin/loop-server-fast.js
+++ bin/loop-server-fast.js
@@ -330,64 +330,6 @@ function parseCookies (request) {
 
 
 function ago(timeStr) {
-   //Time ago
-   // Split timestamp into [ Y, M, D, h, m, s ]
-   /*
-  units = [
-    { name: time.second, plural: time.seconds, limit: 60, in_seconds: 1 },
-    { name: time.minute, plural: time.minutes, limit: 3600, in_seconds: 60 },
-    { name: time.hour, plural: time.hours, limit: 86400, in_seconds: 3600  },
-    { name: time.day, plural: time.days, limit: 604800, in_seconds: 86400 },
-    { name: time.week, plural: time.weeks, limit: 2629743, in_seconds: 604800  },
-    { name: time.month, plural: time.months, limit: 31556926, in_seconds: 2629743 },
-    { name: time.year, plural: time.years, limit: null, in_seconds: 31556926 }
-  ]; */
-  
-  
-  
-  /* Option 2: switch (typeof time) {
-    case 'number': break;
-    case 'string': time = +new Date(time); break;
-    case 'object': if (time.constructor === Date) time = time.getTime(); break;
-    default: time = +new Date();
-  }
-	var time_formats = [
-		[60, 'seconds', 1], // 60
-		[120, '1 minute ago', '1 minute from now'], // 60*2
-		[3600, 'minutes', 60], // 60*60, 60
-		[7200, '1 hour ago', '1 hour from now'], // 60*60*2
-		[86400, 'hours', 3600], // 60*60*24, 60*60
-		[172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
-		[604800, 'days', 86400], // 60*60*24*7, 60*60*24
-		[1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
-		[2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-		[4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
-		[29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-		[58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
-		[2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-		[5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
-		[58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
-	];
-var seconds = (+new Date() - time) / 1000,
-    token = 'ago', list_choice = 1;
-
-if (seconds == 0) {
-    return 'Just now'
-}
-if (seconds < 0) {
-    seconds = Math.abs(seconds);
-    token = 'from now';
-    list_choice = 2;
-}
-var i = 0, format;
-while (format = time_formats[i++])
-    if (seconds < format[0]) {
-        if (typeof format[2] == 'string')
-            return format[list_choice];
-        else
-            return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
-    }
-return time;*/
     
    
    
@@ -409,7 +351,9 @@ return time;*/
   	console.log("unit:" + unit);
     if ((diff < unit.limit) || (!unit.limit)){
       var diff =  Math.floor(diff / unit.in_seconds);
-      return diff + " " + (diff>1 ? unit.plural : unit.name);
+      var timeOut = diff + " " + (diff>1 ? unit.plural : unit.name);
+      console.log(timeOut);
+      return timeOut;
     }
     i++;
   }
