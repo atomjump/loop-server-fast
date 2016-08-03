71812cadc35ea566b204ed90557d16a01fe97500
Apple <apple@Apples-Mac.fritz.box>
2016-08-03 14:55:56 +1200

    first tenative server

--- bin/loop-server-fast.js
+++ bin/loop-server-fast.js
@@ -321,10 +321,16 @@ function parseCookies (request) {
 }
 
 
-function ago(timeStr) {
+function ago(mysqlTimeStr) {
    //Time ago
-  var diff = (new Date() - new Date(timeStr.getTime()*1000)) / 1000;
-  if (diff < 5) return "now";
+   // Split timestamp into [ Y, M, D, h, m, s ]
+var t = "2010-06-09 13:12:01".split(/[- :]/);
+
+ // Apply each element to the Date function
+  var d = new Date(Date.UTC(t[0], t[1]-1, t[2], t[3], t[4], t[5]));
+   
+  //Get diff in seconds 
+  var diff = (new Date() - d) / 1000;
   
   var i = 0, unit;
   while (unit = units[i++]) {
