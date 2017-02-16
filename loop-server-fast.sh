18c4679157ef1b4ebbbaeda3b178a689dcc4ea0d
AtomJump <peter@atomjump.com>
2017-02-16 19:12:03 +1300

    one liner SIGINT trapper

--- loop-server-fast.sh
+++ loop-server-fast.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-exec cd "$(npm prefix -global)/lib/node_modules/loop-server-fast/"; sudo npm run start
+handler()
+{
+    kill -s SIGINT $PID
+}
+
+cd "$(npm prefix -global)/lib/node_modules/loop-server-fast/"; 
+sudo npm run start &
+PID=$!
+
+trap handler SIGINT
+
+wait $PID
+
 #The exec passes on signals directly to the new process
