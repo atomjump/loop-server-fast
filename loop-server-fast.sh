04353b711e1d6abf6fc40c89557b039dfc3d3f27
AtomJump <peter@atomjump.com>
2017-02-16 16:44:38 +1300

    handling sigin smoothly

--- loop-server-fast.sh
+++ loop-server-fast.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+_int() { 
+  echo "Caught SIGINT signal!" 
+  kill -INT "$child" 2>/dev/null
+}
+
+trap _int SIGINT
+
 cd "$(npm prefix -global)/lib/node_modules/loop-server-fast/"
-sudo npm run start
+sudo npm run start &
 
-
+child=$! 
+wait "$child"
