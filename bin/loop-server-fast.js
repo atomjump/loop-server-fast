0b1ee1b6b6ed2175103e4a15f1c1161dda61f2ca
AtomJump <peter@atomjump.com>
2021-03-09 12:24:10 +1300

    reconnect to the individual db if it is not connected or connecting

--- bin/loop-server-fast.js
+++ bin/loop-server-fast.js
@@ -228,7 +228,9 @@ if(cnf.httpsKey) {
  	//       connection: the 2nd array index of connections[][connection]
  	//Optional. If this is a single disconnection, then we can handle this case first and return.
  	console.log("Call to handleDisconnect() Group:" + group + " hostCnt:" + hostCnt);		//TESTING
- 	if(group || hostCnt) {
+ 	if((!group) || (!hostCnt)) {		//If either is not null
+ 		
+ 		{
 		console.log("Attempt a single exit point");		//TESTING
 		return;
 		
